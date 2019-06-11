8072941d0670a9553a28d787f8071dadcd38d488
xiao-ren-wu <l>
2019-06-11 17:47:14 +0800

    disk_sort

---
+++ src/disk_sort/src/org/joywb/config/Config.java
@@ -0,0 +1,15 @@
+package org.joywb.config;
+
+import java.util.Properties;
+
+/**
+ * @author YuWenbo
+ * e-mail 18629015421@163.com
+ * github https://github.com/xiao-ren-wu
+ * @version 1
+ * @since 2019/5/21 8:27
+ */
+
+public class Config {
+
+}
